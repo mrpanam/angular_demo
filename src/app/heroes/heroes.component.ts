72ba4677259735cab0a016a47a18b33a24a863f2
eric paris <mrpanam.p@gmail.com>
2020-11-06 19:58:02 +0100

    v2

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {Hero} from '../hero';
 import {HEROES} from '../mock-heroes';
 import {HeroService} from '../hero.service';
+import {MessageService} from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -13,15 +14,15 @@ export class HeroesComponent implements OnInit {
   heroes : Hero[];
   selectedHero: Hero;
 
-  constructor(private heroService: HeroService) { }
+  constructor(private heroService: HeroService,private messageService: MessageService) { }
 
   ngOnInit(): void {
     this.getHeroes();
   }
 
-  onSelect(hero: Hero): string {
+  onSelect(hero: Hero): void {
     this.selectedHero=hero;
-    return hero.name
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.name}`)
   }
 
   getHeroes(): void {
