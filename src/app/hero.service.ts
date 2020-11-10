5d8dae02327dfcd703e624906d04428794899d9d
eric paris <mrpanam.p@gmail.com>
2020-11-10 20:34:52 +0100

    v3

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -74,7 +74,7 @@ export class HeroService {
     const id = typeof hero === 'number' ? hero : hero.id;
     const url = `${this.heroesUrl}/${id}`;
 
-    return this.http.delete<Hero>(url, this.httpOptions).pipe(
+    return this.http.delete <Hero>(url, this.httpOptions).pipe(
       tap(_ => this.log(`deleted hero id=${id}`)),
       catchError(this.handleError<Hero>('deleteHero'))
     );
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,6 +2,8 @@ import {Component, OnInit} from '@angular/core';
 import {Hero} from '../hero';
 import {HeroService} from '../hero.service';
 import {MessageService} from '../message.service';
+import {BehaviorSubject, Observable, Subject} from 'rxjs';
+import {debounceTime, distinctUntilChanged, startWith, switchMap} from 'rxjs/operators';
 
 @Component({
   selector: 'app-heroes',
@@ -9,15 +11,34 @@ import {MessageService} from '../message.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-
+  heroes$: Observable<Hero[]>;
   heroes: Hero[];
   selectedHero: Hero;
-
+  //private searchTerms = new Subject<string>();
+  private searchTerms2 = new BehaviorSubject('')
   constructor(private heroService: HeroService) {
   }
 
   ngOnInit(): void {
-    this.getHeroes();
+      this.heroes$ = this.searchTerms2.pipe(
+        // wait 300ms after each keystroke before considering the term
+        debounceTime(300),
+        //startWith(''), // <-- start your pipe with an empty string
+
+        // ignore new term if same as previous term
+        distinctUntilChanged(),
+
+        // switch to new search observable each time the term changes
+        switchMap((term: string) => this.heroService.searchHeroes(term)),
+      );
+
+
+    }
+
+  // Push a search term into the observable stream.
+  search(term: string): void {
+    this.searchTerms2.next(term);
+
   }
 
   getHeroes(): void {
@@ -41,6 +62,8 @@ export class HeroesComponent implements OnInit {
     this.heroes = this.heroes.filter(h => h !== hero);
     this.heroService.deleteHero(hero).subscribe();
   }
+
+
 
 
 }
